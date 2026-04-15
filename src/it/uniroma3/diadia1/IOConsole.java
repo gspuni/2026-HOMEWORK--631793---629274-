9d95d41d58dcf76a17416225df0bb60f11fd7d93
Diego Favretto <95187837+drugooo@users.noreply.github.com>
2026-04-15 02:48:44 +0200

    Interfaccia IO e implements in IOConsole

--- src/it/uniroma3/diadia1/IOConsole.java
+++ src/it/uniroma3/diadia1/IOConsole.java
@@ -1,12 +1,12 @@
 package it.uniroma3.diadia1;
 import java.util.Scanner;
-public class IOConsole {
-public void mostraMessaggio(String msg) {
-System.out.println(msg);
-}
-public String leggiRiga() {
-Scanner scannerDiLinee = new Scanner(System.in);
-String riga = scannerDiLinee.nextLine();
-return riga;
-}
+public class IOConsole implements IO {
+	public void mostraMessaggio(String msg) {
+		System.out.println(msg);
+	}
+	public String leggiRiga() {
+		Scanner scannerDiLinee = new Scanner(System.in);
+		String riga = scannerDiLinee.nextLine();
+		return riga;
+	}
 }
